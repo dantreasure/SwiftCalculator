59d426d28728ad2fa04f40a09a618e7df37cdb0b
Daniel Treasure <dbtreasure@gmail.com>
2017-04-27 09:23:10 -0600

    takes project through lecture 2 of video series but in BUG state

---
+++ Calculator/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Calculator
+//
+//  Created by temp on 4/24/17.
+//  Copyright © 2017 Treasure Development. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var bannerColor: UIColor {
+        return UIColor(red: 0.25, green: 0.5, blue: 0.75, alpha: 1.0)
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        view.backgroundColor = .bannerColor
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
-
-
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping{
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
 }
 
